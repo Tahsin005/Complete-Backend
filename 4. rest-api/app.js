86ba79dd8d9e0503b7073a80f3c7fbcd263df3ff
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-05-29 12:42:46 +0600

    rest api - 5

--- 4. rest-api/app.js
+++ 4. rest-api/app.js
@@ -67,6 +67,23 @@ app.put("/update/:id", (req, res) => {
     }
 });
 
+app.delete("/delete/:id", (req, res) => {
+    const findIndexOfCurrentBook = books.findIndex(
+        (item) => item.id === req.params.id
+    );
+    if (findIndexOfCurrentBook !== -1) {
+        const deletedBook = books.splice(findIndexOfCurrentBook, 1);
+        res.status(200).json({
+            message: "Book deleted successfully",
+            data: deletedBook[0],
+        });
+    } else {
+        res.status(404).json({
+            message: "Book not found",
+        });
+    }
+});
+
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
