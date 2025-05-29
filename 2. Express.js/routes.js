f3a6bd5647e8c6d9f2072ac33066a6742696db47
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-05-29 07:03:22 +0600

    express.js - 3

---
+++ 2. Express.js/routes.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the Home Page!');
+});
+
+app.get('/products', (req, res) => {
+    res.json([
+        { id: 1, name: 'Product 1', price: 100 },
+        { id: 2, name: 'Product 2', price: 200 },
+    ]);
+});
+
+app.get('/products/:id', (req, res) => {
+    const productId = req.params.id;
+    const products = [
+        { id: 1, name: 'Product 1', price: 100 },
+        { id: 2, name: 'Product 2', price: 200 },
+    ];
+
+    const getSingleProduct = products.find(product => product.id === parseInt(productId));
+    if (getSingleProduct) {
+        res.json(getSingleProduct);
+    } else {
+        res.status(404).send('Product not found');
+    }
+});
+
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
