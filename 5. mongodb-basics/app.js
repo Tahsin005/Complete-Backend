f15c5befa517aa2b94b6cafa488e3621d92e0a78
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-05-29 23:05:46 +0600

    mongodb basics - 2

--- 5. mongodb-basics/app.js
+++ 5. mongodb-basics/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 
-mongoose.connect('mongodb+srv://asma35dhf:PMtvb842q3pLVQTr@cluster0.91yqb2v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
+mongoose.connect('mongodb+srv://mtfferdous35:YQbOAutEYJN7CmUE@cluster0.7amnsxk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(() => {
     console.log('Database connected');
 }).catch((err) => {
     console.log(err);
@@ -25,17 +25,23 @@ const User = mongoose.model('User', userSchema);
 async function runQueryExample() {
     try {
         // create a new document
-        const newUser = await User({
-            name: 'Ferdous',
-            email: 'ferdous@affpilot.com',
-            age: 20,
-            isActive: true,
-            tags: ['Full Stack Engineer', 'Competitive Programmer', 'Speed Cuber']
-        });
+        // const newUser = await User({
+        //     name: 'Tahsin',
+        //     email: 'tahsin@affpilot.com',
+        //     age: 20,
+        //     isActive: false,
+        //     tags: ['Full Stack Engineer', 'Competitive Programmer', 'Speed Cuber']
+        // });
 
-        await newUser.save();
+        // await newUser.save();
 
-        console.log("Created new user", newUser);
+        // console.log("Created new user", newUser);
+
+        const allUser = await User.find({});
+        console.log("All users:", allUser);
+
+        const activeUsers = await User.find({ isActive: true });
+        console.log("Active users:", activeUsers);
     } catch (error) {
         console.log(error);
     } finally {
