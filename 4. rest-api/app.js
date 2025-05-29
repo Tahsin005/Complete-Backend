20a014548747b2b3b6ca17a29a740dd76c0b656b
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-05-29 10:05:46 +0600

    rest api - 2

--- 4. rest-api/app.js
+++ 4. rest-api/app.js
@@ -25,6 +25,17 @@ app.get("/get", (req, res) => {
     res.json(books);
 });
 
+app.get("/get/:id", (req, res) => {
+    const book = books.find((item) => item.id === req.params.id);
+    if (book) {
+        res.status(200).json(book);
+    } else {
+        res.status(404).json({
+            message: "Book not found! Please try with a different Book ID",
+        });
+    }
+});
+
 const port = 3000;
 
 app.listen(port, () => {
