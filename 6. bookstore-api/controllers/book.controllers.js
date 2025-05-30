a0ff237d9791768b29ef7773d81c77482f28676b
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-05-30 12:07:36 +0600

    bookstore api - 4

--- 6. bookstore-api/controllers/book.controllers.js
+++ 6. bookstore-api/controllers/book.controllers.js
@@ -1,27 +1,37 @@
-const getAllBooks = async (req, res) => {
+const Book = require("../models/book.models.js");
 
-};
+const getAllBooks = async (req, res) => {};
 
-const getBookById = async (req, res) => {
-
-};
+const getBookById = async (req, res) => {};
 
 const addNewBook = async (req, res) => {
-
+    try {
+        const newBookFormData = req.body;
+        const newlyCreatedBook = await Book.create(newBookFormData);
+        if (newBookFormData) {
+            res.status(201).json({
+                success: true,
+                message: "Book added successfully",
+                data: newlyCreatedBook,
+            });
+        }
+    } catch (e) {
+        console.log(e);
+        res.status(500).json({
+            success: false,
+            message: "Something went wrong! Please try again",
+        });
+    }
 };
 
-const updateBookById = async (req, res) => {
+const updateBookById = async (req, res) => {};
 
-};
-
-const deleteBookById = async (req, res) => {
-
-};
+const deleteBookById = async (req, res) => {};
 
 module.exports = {
-    getAllBooks,
-    getBookById,
-    addNewBook,
-    updateBookById,
-    deleteBookById
+  getAllBooks,
+  getBookById,
+  addNewBook,
+  updateBookById,
+  deleteBookById,
 };
