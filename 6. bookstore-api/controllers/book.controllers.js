d1f6b4c6534dc682ce2ac3ed878afb69d054d681
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-05-30 12:13:05 +0600

    bookstore api - 6

--- 6. bookstore-api/controllers/book.controllers.js
+++ 6. bookstore-api/controllers/book.controllers.js
@@ -1,20 +1,46 @@
 const Book = require("../models/book.models.js");
 
 const getAllBooks = async (req, res) => {
+  try {
+    const allBooks = await Book.find({});
+    if (allBooks?.length > 0) {
+      res.status(200).json({
+        success: true,
+        message: "List of books fetched successfully",
+        data: allBooks,
+      });
+    } else {
+      res.status(404).json({
+        success: false,
+        message: "Bo books found in collection",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong! Please try again",
+    });
+  }
+};
+
+const getBookById = async (req, res) => {
     try {
-        const allBooks = await Book.find({});
-        if (allBooks?.length > 0) {
-            res.status(200).json({
-                success: true,
-                message: "List of books fetched successfully",
-                data: allBooks,
-            });
-        } else {
-            res.status(404).json({
+        const getCurrentBookID = req.params.id;
+        const bookDetailsByID = await Book.findById(getCurrentBookID);
+
+        if (!bookDetailsByID) {
+            return res.status(404).json({
                 success: false,
-                message: "Bo books found in collection",
+                message:
+                "Book with the current ID is not found! Please try with a different ID",
             });
         }
+
+        res.status(200).json({
+            success: true,
+            data: bookDetailsByID,
+        });
     } catch (e) {
         console.log(e);
         res.status(500).json({
@@ -23,8 +49,6 @@ const getAllBooks = async (req, res) => {
         });
     }
 };
-
-const getBookById = async (req, res) => {};
 
 const addNewBook = async (req, res) => {
   try {
