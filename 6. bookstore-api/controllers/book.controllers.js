b7165b1d18753e2721d63ee6fd8679d1517d2377
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-05-30 12:09:29 +0600

    bookstore api - 5

--- 6. bookstore-api/controllers/book.controllers.js
+++ 6. bookstore-api/controllers/book.controllers.js
@@ -1,18 +1,18 @@
 const Book = require("../models/book.models.js");
 
-const getAllBooks = async (req, res) => {};
-
-const getBookById = async (req, res) => {};
-
-const addNewBook = async (req, res) => {
+const getAllBooks = async (req, res) => {
     try {
-        const newBookFormData = req.body;
-        const newlyCreatedBook = await Book.create(newBookFormData);
-        if (newBookFormData) {
-            res.status(201).json({
+        const allBooks = await Book.find({});
+        if (allBooks?.length > 0) {
+            res.status(200).json({
                 success: true,
-                message: "Book added successfully",
-                data: newlyCreatedBook,
+                message: "List of books fetched successfully",
+                data: allBooks,
+            });
+        } else {
+            res.status(404).json({
+                success: false,
+                message: "Bo books found in collection",
             });
         }
     } catch (e) {
@@ -22,6 +22,28 @@ const addNewBook = async (req, res) => {
             message: "Something went wrong! Please try again",
         });
     }
+};
+
+const getBookById = async (req, res) => {};
+
+const addNewBook = async (req, res) => {
+  try {
+    const newBookFormData = req.body;
+    const newlyCreatedBook = await Book.create(newBookFormData);
+    if (newBookFormData) {
+      res.status(201).json({
+        success: true,
+        message: "Book added successfully",
+        data: newlyCreatedBook,
+      });
+    }
+  } catch (e) {
+    console.log(e);
+    res.status(500).json({
+      success: false,
+      message: "Something went wrong! Please try again",
+    });
+  }
 };
 
 const updateBookById = async (req, res) => {};
