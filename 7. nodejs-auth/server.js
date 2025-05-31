818d656f156095e2fc00ce594045349e2183fc0b
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-05-31 06:23:22 +0600

    nodejs auth - 8

--- 7. nodejs-auth/server.js
+++ 7. nodejs-auth/server.js
@@ -5,6 +5,7 @@ const app = express();
 const connnectDB = require('./database/db.js');
 const authRoutes = require('./routes/auth.routes.js');
 const homeRoutes = require('./routes/home.routes.js');
+const adminRoutes = require('./routes/admin.routes.js');
 
 const PORT = process.env.PORT || 3000;
 
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/home', homeRoutes);
+app.use('/api/admin', adminRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
