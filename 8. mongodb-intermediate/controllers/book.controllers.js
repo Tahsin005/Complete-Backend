aecc61e1e3f5f5f91eac7737b0bf8b4983a8c8c6
tahsin005 <tahsin.ferdous3546@gmail.com>
2025-06-02 05:15:43 +0600

    mongodb intermediate - 9

--- 8. mongodb-intermediate/controllers/book.controllers.js
+++ 8. mongodb-intermediate/controllers/book.controllers.js
@@ -23,7 +23,7 @@ const createBook = async (req, res) => {
     try {
         const book = new Book(req.body);
         await book.save();
-        
+
         res.status(201).json({
             success: true,
             data: book,
@@ -37,4 +37,30 @@ const createBook = async (req, res) => {
     }
 };
 
-module.exports = { createAuthor, createBook };
+const getBookByAuthor = async (req, res) => {
+    try {
+        const { authorId } = req.params;
+
+        const books = await Book.find({ author: authorId }).populate('author');
+
+        if (!books || books.length === 0) {
+            return res.status(404).json({
+                success: false,
+                message: "No books found for this author",
+            });
+        }
+
+        res.status(200).json({
+            success: true,
+            data: books,
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({
+            success: false,
+            message: "Some error occured",
+        });
+    }
+}
+
+module.exports = { createAuthor, createBook, getBookByAuthor };
--- 8. mongodb-intermediate/routes/book.routes.js
+++ 8. mongodb-intermediate/routes/book.routes.js
@@ -2,11 +2,13 @@ const express = require("express");
 const {
     createAuthor,
     createBook,
+    getBookByAuthor,
 } = require("../controllers/book.controllers.js");
 
 const router = express.Router();
 
 router.post('/author', createAuthor);
 router.post('/book', createBook);
+router.get('/author/:authorId/books', getBookByAuthor);
 
 module.exports = router;
